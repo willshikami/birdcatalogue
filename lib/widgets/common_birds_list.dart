899c4643e73e892e8a739af7cee6688c42b90107
Willard Shikami <willardshikami@gmail.com>
2020-11-10 21:38:26 +0300

    feat: link firebase data

--- lib/widgets/common_birds_list.dart
+++ lib/widgets/common_birds_list.dart
@@ -28,83 +28,83 @@ class _CommonBirdsListState extends State<CommonBirdsList> {
               final listName = doc.data()['birdName'];
               final imgUrl = doc.data()['imageUrl'];
               final scienceName = doc.data()['scientificName'];
-              return Container(
-                child: InkWell(
-                  child: Card(
-                    elevation: 0,
-                    semanticContainer: true,
-                    clipBehavior: Clip.antiAliasWithSaveLayer,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    color: Theme.of(context).splashColor,
-                    child: Container(
-                      width: size.width * 0.4,
-                      // height: size.width * 0.45,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          CachedNetworkImage(
-                            height: size.width * 0.4,
-                            fit: BoxFit.fill,
-                            imageUrl: imgUrl,
-                            errorWidget: (context, url, error) => Center(
-                              child: Column(
-                                children: [
-                                  SizedBox(
-                                    height: 42,
-                                  ),
-                                  Icon(
-                                    Icons.image,
-                                    size: 36,
-                                    color: appTheme().textSelectionHandleColor,
-                                  ),
-                                  SizedBox(
-                                    height: 12,
-                                  ),
-                                  Text(
-                                    'Cant find the image',
-                                    style: TextStyle(
-                                        color: appTheme()
-                                            .textSelectionHandleColor),
-                                  )
-                                ],
-                              ),
-                            ),
-                          ),
-                          Container(
-                            padding: EdgeInsets.all(10),
+              return InkWell(
+                child: Card(
+                  elevation: 0,
+                  semanticContainer: true,
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  color: Theme.of(context).splashColor,
+                  child: Container(
+                    width: size.width * 0.4,
+                    // height: size.width * 0.45,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        CachedNetworkImage(
+                          height: size.width * 0.4,
+                          //TODO: width of image
+                          // width: size.width * 0.4,
+                          fit: BoxFit.fill,
+                          imageUrl: imgUrl,
+                          errorWidget: (context, url, error) => Center(
                             child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text(
-                                  listName,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bodyText1
-                                      .copyWith(
-                                        color: appTheme().textSelectionColor,
-                                      ),
+                                SizedBox(
+                                  height: 42,
+                                ),
+                                Icon(
+                                  Icons.image,
+                                  size: 36,
+                                  color: appTheme().textSelectionHandleColor,
                                 ),
                                 SizedBox(
-                                  height: 2,
+                                  height: 12,
                                 ),
                                 Text(
-                                  scienceName,
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bodyText2
-                                      .copyWith(
-                                        color:
-                                            appTheme().textSelectionHandleColor,
-                                      ),
-                                ),
+                                  'Cant find the image',
+                                  style: TextStyle(
+                                      color: appTheme()
+                                          .textSelectionHandleColor),
+                                )
                               ],
                             ),
                           ),
-                        ],
-                      ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                listName,
+                                overflow: TextOverflow.ellipsis,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyText1
+                                    .copyWith(
+                                      color: appTheme().textSelectionColor,
+                                    ),
+                              ),
+                              SizedBox(
+                                height: 2,
+                              ),
+                              Text(
+                                scienceName,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyText2
+                                    .copyWith(
+                                      color:
+                                          appTheme().textSelectionHandleColor,
+                                    ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
